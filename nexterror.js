1d756ce0a6079d43b5409800b6505b7e457958b9
Evan Martin <martine@danga.com>
2010-02-10 09:43:11 -0800

    better linux support

--- nexterror.js
+++ nexterror.js
@@ -1,13 +1,16 @@
 var spans = document.getElementsByTagName('span');
 var errnum = 0;
-var kMacPathRE = new RegExp('^/b/slave/mac/build/src(.*)$', 'gm');
+// /.../dtoa/dtoa.c:2550: warning: comparison between signed and unsigned
+var kLinuxErrorRE = new RegExp('^[^ :]+:\\d+: ', 'gm');
+var kPathRE = new RegExp('^/b/slave/(mac|linux|linux_view)/build/src(.*)$', 'gm');
 for (var i = 0, span = spans[0]; span = spans[i]; ++i) {
-  if (span.innerHTML.match('error:')) {  // Mac style.
+  if (span.innerHTML.match('error:') ||  // Mac style.
+      span.innerHTML.match(kLinuxErrorRE)) {
     // Don't use createElement/insertBefore because the error message could be in
     // in the middle of a large span, and we want the anchor right here.
     var anchor = '<a name=error' + errnum++ + '></a>';
-    span.innerHTML = span.innerHTML.replace(/(error:)/g, anchor + '<font color=red>$1</font>');
-    span.innerHTML = span.innerHTML.replace(kMacPathRE, '...<b>$1</b>');
+    span.innerHTML = span.innerHTML.replace(/(error:|warning:)/g, anchor + '<font color=red>$1</font>');
+    span.innerHTML = span.innerHTML.replace(kPathRE, '...<b>$2</b>');
   }
 }
 var errorcount = errnum;
@@ -28,7 +31,7 @@ function nextError() {
 // Construct the button and show it.
 var next = document.createElement('input');
 next.type = 'submit';
-next.value = 'next error';
+next.value = 'next error (' + errorcount + ' found)';
 next.style.position = 'fixed';
 next.style.right = 0;
 next.style.top = 0;

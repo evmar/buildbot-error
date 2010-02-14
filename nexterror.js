54b192b10f2786cce2b02cc953596a2508a028c9
Finnur Thorarinsson <finnur@google.com>
2010-02-14 21:47:10 +0100

    windows support, ui improvements
    - Added support for Windows errors.
    - Made the button traverse each error (not just first within a span).
    - Changed the button label to say "Error n/m" once you start traversing so you
      know exactly where in the list you are.
    - Made the error anchor labelling 1-based, since we show the anchor in the URL
      (and use the label in the button as well).

--- nexterror.js
+++ nexterror.js
@@ -3,37 +3,66 @@ var errnum = 0;
 // /.../dtoa/dtoa.c:2550: warning: comparison between signed and unsigned
 var kLinuxErrorRE = new RegExp('^[^ :]+:\\d+: ', 'gm');
 var kPathRE = new RegExp('^/b/slave/(mac|linux|linux_view)/build/src(.*)$', 'gm');
-for (var i = 0, span = spans[0]; span = spans[i]; ++i) {
+var kPathWinRE = new RegExp('[a-zA-Z]:\\\\b\\\\slave\\\\([^\\\\]*)\\\\build\\\\src\\\\(.*)$', 'gm');
+for (var i = 0; i < spans.length; ++i) {
+  var span = spans[i];
   if (span.innerHTML.match('error:') ||  // Mac style.
+      span.innerHTML.match('error C[0-9][0-9][0-9][0-9]') ||  // Windows style.
       span.innerHTML.match(kLinuxErrorRE)) {
-    // Don't use createElement/insertBefore because the error message could be in
-    // in the middle of a large span, and we want the anchor right here.
-    var anchor = '<a name=error' + errnum++ + '></a>';
-    span.innerHTML = span.innerHTML.replace(/(error:|warning:)/g, anchor + '<font color=red>$1</font>');
     span.innerHTML = span.innerHTML.replace(kPathRE, '...<b>$2</b>');
+    span.innerHTML = span.innerHTML.replace(kPathWinRE, '...\\<b>$2</b>');
+    while (true) {
+      // Don't use createElement/insertBefore because the error message could be in
+      // in the middle of a large span, and we want the anchor right here.
+      var anchor = '<a name=error' + ++errnum + '></a>';
+      var length = span.innerHTML.length;
+      // We insert an anchor before the error message and if we don't exclude
+      // errors/warnings with </a> in front this will loop forever, trying to
+      // insert the anchor tag. Hence the [^>] at the front of the regexp.
+      span.innerHTML = span.innerHTML.replace(/[^>](error:|error C[0-9][0-9][0-9][0-9]|warning:)/, anchor + '<font color=red>$1</font>');
+      if (span.innerHTML.length == length)
+        break;  // Nothing left to replace.
+    }
   }
 }
-var errorcount = errnum;
+
+var errorcount = Math.max(errnum - 1, 0);
+var index = currentIndex();
+
+// Returns the one-based index of current error.
+function currentIndex() {
+  var hash = document.location.hash;
+  if (!hash)
+    return 0;
+  else
+    return parseInt(hash.substr('#error'.length));
+}
+
+function buttonLabel(index) {
+  if (errorcount == 0)
+    return 'Error: ?';
+  else if (index == 0)
+    return 'Next error (' + errorcount + ' found)';
+  else
+    return 'Error ' + index + '/' + errorcount;
+}
 
 function nextError() {
-  var hash = document.location.hash;
-  if (!hash) {
-    document.location.hash = 'error0';
-  } else {
-    var err = parseInt(document.location.hash.substr('#error'.length));
-    if (++err == errorcount || isNaN(err))
-      err = 0;
-    document.location.hash = 'error' + err;
-  }
+  var index = currentIndex();
+  if (++index > errorcount || isNaN(index))
+    index = 1;
+  document.location.hash = 'error' + index;
+  document.getElementById('nextErrorButton').value = buttonLabel(index);
 }
-
 
 // Construct the button and show it.
 var next = document.createElement('input');
+next.id = 'nextErrorButton';
 next.type = 'submit';
-next.value = 'next error (' + errorcount + ' found)';
+next.value = buttonLabel(index);
 next.style.position = 'fixed';
 next.style.right = 0;
 next.style.top = 0;
 next.onclick = nextError;
 document.body.appendChild(next);
+

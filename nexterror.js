e924e7204e01aa2d86508b44ef816cf7a6f3f0c6
Evan Martin <martine@danga.com>
2010-08-03 14:51:40 -0700

    wrap in function

--- nexterror.js
+++ nexterror.js
@@ -1,48 +1,51 @@
-var spans = document.getElementsByTagName('span');
-var next_error = 1;
-var next_warning = 1;
-// /.../dtoa/dtoa.c:2550: warning: comparison between signed and unsigned
-var kGCCErrorRE = new RegExp('^[^ :]+:\\d+: ', 'gm');
-var kPathRE = new RegExp('^/b/slave/(mac|linux|linux_view)/build/src(.*)$', 'gm');
-var kPathWinRE = new RegExp('[a-zA-Z]:\\\\b\\\\slave\\\\([^\\\\]*)\\\\build\\\\src\\\\(.*)$', 'gm');
-for (var i = 0; i < spans.length; ++i) {
-  var span = spans[i];
-  if (span.innerHTML.match('error:') ||  // Mac style.
-      span.innerHTML.match('error C[0-9][0-9][0-9][0-9]') ||  // Windows style.
-      span.innerHTML.match(kGCCErrorRE)) {
-    span.innerHTML = span.innerHTML.replace(kPathRE, '...<b>$2</b>');
-    span.innerHTML = span.innerHTML.replace(kPathWinRE, '...\\<b>$2</b>');
-    while (true) {
-      // Don't use createElement/insertBefore because the error message could be in
-      // in the middle of a large span, and we want the anchor right here.
-      var length = span.innerHTML.length;
-      // We insert an anchor before the error message and if we don't exclude
-      // errors/warnings with </a> in front this will loop forever, trying to
-      // insert the anchor tag. Hence the [^>] at the front of the regexp.
-      span.innerHTML = span.innerHTML.replace(/[^>](error:|error C[0-9][0-9][0-9][0-9])/,
-                                              '<a name=error' + next_error + '></a>' +
-                                              '<font color=red>$1</font>');
-      if (span.innerHTML.length != length) {
-        ++next_error;
-        continue;
-      }
+var errorcount = 0;
 
-      span.innerHTML = span.innerHTML.replace(/[^>](warning:)/,
-                                              '<a name=error' + next_warning + '></a>' +
-                                              '<font color=red>$1</font>');
-      if (span.innerHTML.length != length) {
-        ++next_warning;
-        continue;
-      }
+function doParse() {
+  var next_error = 1;
+  var next_warning = 1;
+  var spans = document.getElementsByTagName('span');
+  // /.../dtoa/dtoa.c:2550: warning: comparison between signed and unsigned
+  var kGCCErrorRE = new RegExp('^[^ :]+:\\d+: ', 'gm');
+  var kPathRE = new RegExp('^/b/slave/(mac|linux|linux_view)/build/src(.*)$', 'gm');
+  var kPathWinRE = new RegExp('[a-zA-Z]:\\\\b\\\\slave\\\\([^\\\\]*)\\\\build\\\\src\\\\(.*)$', 'gm');
+  for (var i = 0; i < spans.length; ++i) {
+    var span = spans[i];
+    if (span.innerHTML.match('error:') ||  // Mac style.
+        span.innerHTML.match('error C[0-9][0-9][0-9][0-9]') ||  // Windows style.
+        span.innerHTML.match(kGCCErrorRE)) {
+      span.innerHTML = span.innerHTML.replace(kPathRE, '...<b>$2</b>');
+      span.innerHTML = span.innerHTML.replace(kPathWinRE, '...\\<b>$2</b>');
+      while (true) {
+        // Don't use createElement/insertBefore because the error message could be in
+        // in the middle of a large span, and we want the anchor right here.
+        var length = span.innerHTML.length;
+        // We insert an anchor before the error message and if we don't exclude
+        // errors/warnings with </a> in front this will loop forever, trying to
+        // insert the anchor tag. Hence the [^>] at the front of the regexp.
+        span.innerHTML = span.innerHTML.replace(/[^>](error:|error C[0-9][0-9][0-9][0-9])/,
+                                                '<a name=error' + next_error + '></a>' +
+                                                '<font color=red>$1</font>');
+        if (span.innerHTML.length != length) {
+          ++next_error;
+          continue;
+        }
 
-      // If we get here, there's nothing left to replace.
-      break;
+        span.innerHTML = span.innerHTML.replace(/[^>](warning:)/,
+                                                '<a name=warning' + next_warning + '></a>' +
+                                                '<font color=red>$1</font>');
+        if (span.innerHTML.length != length) {
+          ++next_warning;
+          continue;
+        }
+
+        // If we get here, there's nothing left to replace.
+        break;
+      }
     }
   }
+  errorcount = next_error - 1;
 }
 
-var errorcount = next_error - 1;
-var index = currentIndex();
 
 // Returns the one-based index of current error.
 function currentIndex() {
@@ -70,11 +73,13 @@ function nextError() {
   document.getElementById('nextErrorButton').value = buttonLabel(index);
 }
 
+doParse();
+
 // Construct the button and show it.
 var next = document.createElement('input');
 next.id = 'nextErrorButton';
 next.type = 'submit';
-next.value = buttonLabel(index);
+next.value = buttonLabel(currentIndex());
 next.style.position = 'fixed';
 next.style.right = 0;
 next.style.top = 0;
